8f7e8ffcf29c4add83d4a80af196ee58d2d820b1
danieljdd <danieljdijkstra@gmail.com>
2024-06-26 10:58:28 +0200

    Remove intento submodule configuration

--- intento/x/intent/types/events.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// Governance module event types
-const (
-	AttributeKeyActionOwner = "owner"
-	EventTypeAction         = "action"
-	//EventTypeActionExpired  = "action-expired"
-	AttributeKeyActionID = "tx-id"
-)
